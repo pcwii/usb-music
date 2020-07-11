adf655def1d68c14dd8b9fa0d17bc4fc70727f44
philc <philc@arcx.com>
2020-07-10 22:10:22 -0400

    Changed library merge options
    Change-Id: Ia89cf88cab28370af300fcd56b0d169f00f742f4

--- __init__.py
+++ __init__.py
@@ -399,7 +399,6 @@ class USBMusicSkill(CommonPlaySkill):
 
     def merge_library(self, dict1, dict2):
         return dict1 + dict2
-        #return dict2.update(dict1)
 
     def stop(self):
         if self.audio_state == 'playing':
