464243f64d095899d6ebb1be3eb27d3741052e5f
Phil Chenier <philc@arcx.com>
2020-08-24 15:05:12 -0400

    changed audio service to vlc

--- __init__.py
+++ __init__.py
@@ -281,8 +281,8 @@ class USBMusicSkill(CommonPlaySkill):
         new_library = []
         for root, d_names, f_names in os.walk(str(source_path)):
             for fileName in f_names:
-                if ("mp3" or "flac") in str(fileName):
-                # if ("flac") in str(fileName):
+                #if ("mp3" or "flac") in str(fileName):
+                if ("flac") in str(fileName):
                     song_path = str(root) + "/" + str(fileName)
                     try:
                         if "flac" in str(fileName):  # add flac filter
